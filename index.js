6faaf9e06900a9335c5f1ea6e9e8ccad03383713
ValHalla358 <vcs358@gmail.com>
2017-01-17 13:20:20 -0800

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-$(function() {
+// $(document).ready(function() {
+window.initMap = function() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: { lat: 32.715736, lng: -117.161087 }
+    });
+
     $.getJSON("topspots.json", function(result) {
         $(result).each(function(index, value) {
             var row = ``;
@@ -6,9 +13,17 @@ $(function() {
             mapLink += value.location[0] + ',' + value.location[1];
             row += `<tr><td>${value.name}</td><td>${value.description}</td><td class="btn btn-default"><a href= "${mapLink}" >Location</a></td></tr>`;
             $("tbody").append(row);
+
+
+            var marker = new google.maps.Marker({
+                position: { lat: value.location[0], lng: value.location[1] },
+                map: map
+            });
+
         });
 
-
     });
+};
+// });
 
-});
+
